1b7be52311a27d2b3bb3066a0e455f2f469867e7
Yannik Marchand <ymarchand@me.com>
2022-11-09 15:11:49 +0100

    Push version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = \
 
 setuptools.setup(
 	name = "anynet",
-	version = "0.0.14",
+	version = "0.0.15",
 	description = "Networking library based on anyio",
 	long_description = long_description,
 	author = "Yannik Marchand",
